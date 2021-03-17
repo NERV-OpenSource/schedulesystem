95da9757cbf1a11d3ea56661a5ca4edeb8ceb891
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2021-03-16 23:28:04 -0300

    SystemSchedule Update
    Added more features

--- src/main.js
+++
@@ -1,7 +0,0 @@
-var app = new Vue({
-    el: '#app',
-    data:{
-        teste: 'Teste',
-        usuario: 'Guilherme Araujo'
-    }
-})
---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        test: 'Teste',
+        user: 'Guilherme Araujo'
+    }
+})
