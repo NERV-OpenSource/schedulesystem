31965c9ba280897448277989f69c9ac48dfec05f
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2021-03-19 11:14:39 -0300

    ScheduleSystem

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+import '../public/styles/global.css'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
