0b6df97ab93a9b10991ac16944be52bb2ac9f0cf
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2021-04-08 15:28:36 -0300

    ScheduleSystem
    Added Login page, trying to put routes as well

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      Oi
+  </div>
+</template>
+
+
+
+
+<script>
+import "../../public/styles/home.css";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '../components/Login';
+import Home from '../components/Home';
+
+
+Vue.use(Router);
+
+
+export default new Router({
+    routes:[
+        {
+            path: "/",
+            redirect:{
+                name: "Login"
+            }
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        }
+       
+    ]
+})
